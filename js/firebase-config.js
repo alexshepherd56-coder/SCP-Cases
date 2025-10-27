dfdb519613105ba5a7dff8575241227b7ef95426
alexshepherd56-coder <alex.shepherd56@gmail.com>
2025-10-27 12:23:08 +1100

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLbZmhAcXAwKLfKZ7-xnKt_TX0kpyNGV4",
+  authDomain: "scp-case-bank.firebaseapp.com",
+  projectId: "scp-case-bank",
+  storageBucket: "scp-case-bank.firebasestorage.app",
+  messagingSenderId: "1023483730503",
+  appId: "1:1023483730503:web:651c44095fbeecbf6b2f62",
+  measurementId: "G-MT2L4GYWE4"
+};
